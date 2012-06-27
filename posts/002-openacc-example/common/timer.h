5b2a8e3d29147c1250dfeb7d34899507f83bf2d7
Mark Harris <harrism@gmail.com>
2012-06-27 16:19:53 +1000

    fixed license text in common.h

--- posts/002-openacc-example/common/timer.h
+++ posts/002-openacc-example/common/timer.h
@@ -1,12 +1,17 @@
 /*
- * Copyright 1993-2012 NVIDIA Corporation.  All rights reserved.
+ *  Copyright 2012 NVIDIA Corporation
  *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 #ifndef TIMER_H
