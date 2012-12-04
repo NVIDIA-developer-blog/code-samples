661cc78a205ba4a0a7590ee6fb4cd2a43236059d
Mark Harris <harrism@gmail.com>
2012-12-04 21:37:02 +1000

    Added source code for Introduction to CUDA C++ series post on optimizing data transfers

---
+++ series/cuda-cpp/optimize-data-transfers/profile.cu
@@ -0,0 +1,14 @@
+int main()
+{
+    const unsigned int N = 1048576;
+    const unsigned int bytes = N * sizeof(int);
+    int *h_a = (int*)malloc(bytes);
+    int *d_a;
+    cudaMalloc((int**)&d_a, bytes);
+
+    memset(h_a, 0, bytes);
+    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(h_a, d_a, bytes, cudaMemcpyDeviceToHost);
+
+    return 0;
+}
