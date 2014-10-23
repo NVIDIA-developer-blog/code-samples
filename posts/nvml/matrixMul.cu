3446e51dfc5421d8194c3c923cfb24952c704399
Jiri Kraus <jkraus@nvidia.com>
2014-10-23 07:28:40 -0700

    Added NVML example.

---
+++ posts/nvml/matrixMul.cu
@@ -0,0 +1,342 @@
+/**
+ * Copyright 1993-2012 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+/**
+ * Matrix multiplication: C = A * B.
+ * Host code.
+ *
+ * This sample implements matrix multiplication as described in Chapter 3
+ * of the programming guide.
+ * It has been written for clarity of exposition to illustrate various CUDA
+ * programming principles, not with the goal of providing the most
+ * performant generic kernel for matrix multiplication.
+ *
+ * See also:
+ * V. Volkov and J. Demmel, "Benchmarking GPUs to tune dense linear algebra,"
+ * in Proc. 2008 ACM/IEEE Conf. on Superconducting (SC '08),
+ * Piscataway, NJ: IEEE Press, 2008, pp. Art. 31:1-11.
+ */
+
+// System includes
+#include <stdio.h>
+#include <assert.h>
+
+// CUDA runtime
+#include <cuda_runtime.h>
+
+// Helper functions and utilities to work with CUDA
+//#include <helper_functions.h>
+
+/**
+ * Matrix multiplication (CUDA Kernel) on the device: C = A * B
+ * wA is A's width and wB is B's width
+ */
+template <int BLOCK_SIZE> __global__ void
+matrixMulCUDA(float *C, float *A, float *B, int wA, int wB)
+{
+    // Block index
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+
+    // Thread index
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    // Index of the first sub-matrix of A processed by the block
+    int aBegin = wA * BLOCK_SIZE * by;
+
+    // Index of the last sub-matrix of A processed by the block
+    int aEnd   = aBegin + wA - 1;
+
+    // Step size used to iterate through the sub-matrices of A
+    int aStep  = BLOCK_SIZE;
+
+    // Index of the first sub-matrix of B processed by the block
+    int bBegin = BLOCK_SIZE * bx;
+
+    // Step size used to iterate through the sub-matrices of B
+    int bStep  = BLOCK_SIZE * wB;
+
+    // Csub is used to store the element of the block sub-matrix
+    // that is computed by the thread
+    float Csub = 0;
+
+    // Loop over all the sub-matrices of A and B
+    // required to compute the block sub-matrix
+    for (int a = aBegin, b = bBegin;
+         a <= aEnd;
+         a += aStep, b += bStep)
+    {
+
+        // Declaration of the shared memory array As used to
+        // store the sub-matrix of A
+        __shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+
+        // Declaration of the shared memory array Bs used to
+        // store the sub-matrix of B
+        __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
+
+        // Load the matrices from device memory
+        // to shared memory; each thread loads
+        // one element of each matrix
+        As[ty][tx] = A[a + wA * ty + tx];
+        Bs[ty][tx] = B[b + wB * ty + tx];
+
+        // Synchronize to make sure the matrices are loaded
+        __syncthreads();
+
+        // Multiply the two matrices together;
+        // each thread computes one element
+        // of the block sub-matrix
+#pragma unroll
+
+        for (int k = 0; k < BLOCK_SIZE; ++k)
+        {
+            Csub += As[ty][k] * Bs[k][tx];
+        }
+
+        // Synchronize to make sure that the preceding
+        // computation is done before loading two new
+        // sub-matrices of A and B in the next iteration
+        __syncthreads();
+    }
+
+    // Write the block sub-matrix to device memory;
+    // each thread writes one element
+    int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+    C[c + wB * ty + tx] = Csub;
+}
+
+void constantInit(float *data, int size, float val)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        data[i] = val;
+    }
+}
+
+/**
+ * Run a simple test of matrix multiplication using CUDA
+ */
+int matrixMultiply(dim3 &dimsA, dim3 &dimsB)
+{
+	int block_size = 32;
+    // Allocate host memory for matrices A and B
+    unsigned int size_A = dimsA.x * dimsA.y;
+    unsigned int mem_size_A = sizeof(float) * size_A;
+    float *h_A = (float *)malloc(mem_size_A);
+    unsigned int size_B = dimsB.x * dimsB.y;
+    unsigned int mem_size_B = sizeof(float) * size_B;
+    float *h_B = (float *)malloc(mem_size_B);
+
+    // Initialize host memory
+    const float valB = 0.01f;
+    constantInit(h_A, size_A, 1.0f);
+    constantInit(h_B, size_B, valB);
+
+    // Allocate device memory
+    float *d_A, *d_B, *d_C;
+
+    // Allocate host matrix C
+    dim3 dimsC(dimsB.x, dimsA.y, 1);
+    unsigned int mem_size_C = dimsC.x * dimsC.y * sizeof(float);
+    float *h_C = (float *) malloc(mem_size_C);
+
+    if (h_C == NULL)
+    {
+        fprintf(stderr, "Failed to allocate host matrix C!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    cudaError_t error;
+
+    error = cudaMalloc((void **) &d_A, mem_size_A);
+
+    if (error != cudaSuccess)
+    {
+        printf("cudaMalloc d_A returned error code %d, line(%d)\n", error, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    error = cudaMalloc((void **) &d_B, mem_size_B);
+
+    if (error != cudaSuccess)
+    {
+        printf("cudaMalloc d_B returned error code %d, line(%d)\n", error, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    error = cudaMalloc((void **) &d_C, mem_size_C);
+
+    if (error != cudaSuccess)
+    {
+        printf("cudaMalloc d_C returned error code %d, line(%d)\n", error, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    // copy host memory to device
+    error = cudaMemcpy(d_A, h_A, mem_size_A, cudaMemcpyHostToDevice);
+
+    if (error != cudaSuccess)
+    {
+        printf("cudaMemcpy (d_A,h_A) returned error code %d, line(%d)\n", error, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    error = cudaMemcpy(d_B, h_B, mem_size_B, cudaMemcpyHostToDevice);
+
+    if (error != cudaSuccess)
+    {
+        printf("cudaMemcpy (d_B,h_B) returned error code %d, line(%d)\n", error, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    // Setup execution parameters
+    dim3 threads(block_size, block_size);
+    dim3 grid(dimsB.x / threads.x, dimsA.y / threads.y);
+
+    // Create and start timer
+    printf("\tRunning Matrix Multiplication CUDA Kernel...\n");
+
+    // Performs warmup operation using matrixMul CUDA kernel
+    if (block_size == 16)
+    {
+        matrixMulCUDA<16><<< grid, threads >>>(d_C, d_A, d_B, dimsA.x, dimsB.x);
+    }
+    else
+    {
+        matrixMulCUDA<32><<< grid, threads >>>(d_C, d_A, d_B, dimsA.x, dimsB.x);
+    }
+
+    cudaDeviceSynchronize();
+
+    // Allocate CUDA events that we'll use for timing
+    cudaEvent_t start;
+    error = cudaEventCreate(&start);
+
+    if (error != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to create start event (error code %s)!\n", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
+    cudaEvent_t stop;
+    error = cudaEventCreate(&stop);
+
+    if (error != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to create stop event (error code %s)!\n", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
+    // Record the start event
+    error = cudaEventRecord(start, NULL);
+
+    if (error != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to record start event (error code %s)!\n", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
+    // Execute the kernel
+    int nIter = 300;
+
+    for (int j = 0; j < nIter; j++)
+    {
+        if (block_size == 16)
+        {
+            matrixMulCUDA<16><<< grid, threads >>>(d_C, d_A, d_B, dimsA.x, dimsB.x);
+        }
+        else
+        {
+            matrixMulCUDA<32><<< grid, threads >>>(d_C, d_A, d_B, dimsA.x, dimsB.x);
+        }
+    }
+
+    // Record the stop event
+    error = cudaEventRecord(stop, NULL);
+
+    if (error != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to record stop event (error code %s)!\n", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
+    // Wait for the stop event to complete
+    error = cudaEventSynchronize(stop);
+
+    if (error != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to synchronize on the stop event (error code %s)!\n", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
+    float msecTotal = 0.0f;
+    error = cudaEventElapsedTime(&msecTotal, start, stop);
+
+    if (error != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to get time elapsed between events (error code %s)!\n", cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+
+    // Compute and print the performance
+    float msecPerMatrixMul = msecTotal / nIter;
+    double flopsPerMatrixMul = 2.0 * (double)dimsA.x * (double)dimsA.y * (double)dimsB.x;
+    double gigaFlops = (flopsPerMatrixMul * 1.0e-9f) / (msecPerMatrixMul / 1000.0f);
+    printf(
+        "\tPerformance= %.2f GFlop/s, Time= %.3f msec, Size= %.0f Ops\n",
+        gigaFlops,
+        msecPerMatrixMul,
+        flopsPerMatrixMul);
+
+    // Copy result from device to host
+    error = cudaMemcpy(h_C, d_C, mem_size_C, cudaMemcpyDeviceToHost);
+
+    if (error != cudaSuccess)
+    {
+        printf("cudaMemcpy (h_C,d_C) returned error code %d, line(%d)\n", error, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("\tChecking computed result for correctness: ");
+    bool correct = true;
+
+    for (int i = 0; i < (int)(dimsC.x * dimsC.y); i++)
+    {
+        if (fabs(h_C[i] - (dimsA.x * valB)) > 1e-5)
+        {
+            printf("Error! Matrix[%05d]=%.8f, ref=%.8f error term is > 1e-5\n", i, h_C[i], dimsA.x*valB);
+            correct = false;
+        }
+    }
+
+    printf("%s\n", correct ? "OK" : "FAIL");
+
+    // Clean up memory
+    free(h_A);
+    free(h_B);
+    free(h_C);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    printf("\n\tNote: For peak performance, please refer to the matrixMulCUBLAS example.\n");
+
+    if (correct)
+    {
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        return EXIT_FAILURE;
+    }
+}
---
+++ posts/nvml/nvml_test.cu
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <cstdio>
+
+#include <nvml.h>
+#define NVML_CALL( call )				\
+{										\
+	nvmlReturn_t nvmlError = call;		\
+	if (NVML_SUCCESS != nvmlError )	{	\
+		fprintf (stderr, "NVML_ERROR: %s (%d) in %d line of %s\n", nvmlErrorString( nvmlError ), nvmlError , __LINE__, __FILE__ ); \
+	}									\
+}
+
+/**
+ * getNvmlDevice determines the NVML Device Id of the currently active CUDA device
+ *
+ * @param[out]  nvmlDeviceId    the NVML Device Id of the currently active CUDA device
+ * @return                      NVML_SUCCESS in case of success. Error code of NVML API
+ *                              or NVML_ERROR_UNKNOWN if CUDA Runtime API failed otherwise
+ */
+inline nvmlReturn_t getNvmlDevice( nvmlDevice_t* nvmlDeviceId )
+{
+	int activeCUDAdevice = 0;
+	cudaError_t cudaError = cudaGetDevice ( &activeCUDAdevice );
+	if ( cudaSuccess  != cudaError )
+		return NVML_ERROR_UNKNOWN;
+	
+	cudaDeviceProp activeCUDAdeviceProp;
+	cudaError = cudaGetDeviceProperties ( &activeCUDAdeviceProp, activeCUDAdevice );
+	if ( cudaSuccess  != cudaError )
+		return NVML_ERROR_UNKNOWN;
+	
+	unsigned int nvmlDeviceCount = 0;
+	nvmlReturn_t nvmlError = nvmlDeviceGetCount ( &nvmlDeviceCount );
+	if ( NVML_SUCCESS != nvmlError )
+		return nvmlError;
+	
+	for ( unsigned int nvmlDeviceIdx = 0; nvmlDeviceIdx < nvmlDeviceCount; ++nvmlDeviceIdx )
+	{
+		nvmlError = nvmlDeviceGetHandleByIndex ( nvmlDeviceIdx, nvmlDeviceId );
+		if ( NVML_SUCCESS != nvmlError )
+			return nvmlError; 
+		nvmlPciInfo_t nvmPCIInfo;
+		nvmlError = nvmlDeviceGetPciInfo ( *nvmlDeviceId, &nvmPCIInfo );
+		if ( NVML_SUCCESS != nvmlError )
+			return nvmlError;
+		if ( static_cast<unsigned int>(activeCUDAdeviceProp.pciBusID) == nvmPCIInfo.bus &&
+		     static_cast<unsigned int>(activeCUDAdeviceProp.pciDeviceID) == nvmPCIInfo.device &&
+			 static_cast<unsigned int>(activeCUDAdeviceProp.pciDomainID) == nvmPCIInfo.domain )
+			break;
+	}
+	return NVML_SUCCESS;
+}
+
+inline nvmlReturn_t reportApplicationClocks( nvmlDevice_t nvmlDeviceId )
+{
+	unsigned int appSMclock = 0;
+	unsigned int appMemclock = 0;
+	nvmlReturn_t nvmlError = nvmlDeviceGetApplicationsClock ( nvmlDeviceId, NVML_CLOCK_SM, &appSMclock );
+	if ( NVML_SUCCESS != nvmlError )
+			return nvmlError;
+	nvmlError = nvmlDeviceGetApplicationsClock ( nvmlDeviceId, NVML_CLOCK_MEM, &appMemclock );
+	if ( NVML_SUCCESS != nvmlError )
+			return nvmlError;
+	
+	std::cout<<"Application Clocks = ("<<appMemclock<<","<<appSMclock<<")"<<std::endl;
+	return NVML_SUCCESS;
+}
+
+int matrixMultiply(dim3 &dimsA, dim3 &dimsB);
+
+int main()
+{
+	cudaSetDevice(0);
+	cudaFree(0);
+	
+	NVML_CALL( nvmlInit() );
+	
+	nvmlDevice_t nvmlDeviceId;
+	NVML_CALL( getNvmlDevice( &nvmlDeviceId ) );
+	
+	unsigned int memClock = 0;
+	NVML_CALL( nvmlDeviceGetClockInfo( nvmlDeviceId, NVML_CLOCK_MEM, &memClock ) );
+	
+	unsigned int numSupportedSMClocks = 32;
+	unsigned int smClocksMHz[32];
+	NVML_CALL( nvmlDeviceGetSupportedGraphicsClocks ( nvmlDeviceId, memClock, &numSupportedSMClocks, smClocksMHz ) );
+	
+	unsigned int numSupportedMemClocks = 32;
+	unsigned int memClocksMHz[32];
+	NVML_CALL( nvmlDeviceGetSupportedMemoryClocks ( nvmlDeviceId, &numSupportedMemClocks, memClocksMHz ) ); 
+
+	unsigned int maxSMclock = 0;
+	unsigned int maxMemclock = 0;
+	NVML_CALL( nvmlDeviceGetMaxClockInfo ( nvmlDeviceId, NVML_CLOCK_SM, &maxSMclock ) );
+	NVML_CALL( nvmlDeviceGetMaxClockInfo ( nvmlDeviceId, NVML_CLOCK_MEM, &maxMemclock ) );
+
+	//Check permissions to modify application clocks
+	nvmlEnableState_t isRestricted;
+	NVML_CALL( nvmlDeviceGetAPIRestriction ( nvmlDeviceId, NVML_RESTRICTED_API_SET_APPLICATION_CLOCKS, &isRestricted ) );
+	
+	if ( NVML_FEATURE_DISABLED == isRestricted )
+	{
+		dim3 dimsA(256,256);
+		dim3 dimsB(256,256);
+		
+		for ( int i=numSupportedSMClocks-1; i >= 0; --i )
+		{
+			std::cout<<"Setting ";
+			NVML_CALL( nvmlDeviceSetApplicationsClocks ( nvmlDeviceId, memClocksMHz[0], smClocksMHz[i] ) );
+			
+			NVML_CALL( reportApplicationClocks( nvmlDeviceId ) );
+			
+			matrixMultiply(dimsA, dimsB);
+		}
+	}
+	
+	//Reset Application Clocks and Shutdown NVML
+	if ( NVML_FEATURE_DISABLED == isRestricted )
+	{
+		NVML_CALL( nvmlDeviceResetApplicationsClocks ( nvmlDeviceId ) ); 
+	}
+	NVML_CALL( nvmlShutdown() );
+	
+	cudaDeviceReset();
+	return 0;
+}
