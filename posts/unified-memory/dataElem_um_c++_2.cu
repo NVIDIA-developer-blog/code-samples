b177d3d01a355ed27e561bccfb99122e8e760cd0
Mark Harris <harrism@gmail.com>
2015-06-12 10:00:03 +1000

    Added missing cudaDeviceSynchronize()

--- posts/unified-memory/dataElem_um_c++_2.cu
+++ posts/unified-memory/dataElem_um_c++_2.cu
@@ -9,10 +9,12 @@ public:
   void *operator new(size_t len) {
     void *ptr;
     cudaMallocManaged(&ptr, len);
+    cudaDeviceSynchronize();
     return ptr;
   }
 
   void operator delete(void *ptr) {
+    cudaDeviceSynchronize();
     cudaFree(ptr);
   }
 };
